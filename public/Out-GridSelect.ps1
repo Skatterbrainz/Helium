dd66e80a4473a0eefd88f616d93d6593204b3f0e
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-03-06 11:55:09 -0500

    1.1.11

--- public/Out-GridSelect.ps1
+++ public/Out-GridSelect.ps1
@@ -1,20 +1,61 @@
+<#
+.SYNOPSIS
+Use GridView or ConsoleGridView to provide a selection menu
+
+.DESCRIPTION
+Use a GridView or ConsoleGridView to provide a selection menu interface for selecting either
+single or multiple items. On Linux this will only invoke a ConsoleGridView. For Windows, it 
+will default to a GridView unless -ForceConsole is used.
+
+.PARAMETER InputObject
+An array of objects.
+
+.PARAMETER Title
+Title to display on the gridview heading (caption). If not provided will display "Select Item"
+or "Select Items"
+
+.PARAMETER OutputMode
+Optional. Single (Default) or Multiple.
+
+.PARAMETER ForceConsole
+Optional. On Windows machines, forces ConsoleGridView
+
+.EXAMPLE
+Get-Process | Out-GridSelect
+
+.EXAMPLE
+Get-Process | Out-GridSelect -Title "Select Processes" -OutputMode Multiple
+
+.NOTES
+The Microsoft.PowerShell.ConsoleGuiTools module is required on Linux, or when using -ForceConsole.
+
+.LINK
+#>
 function Out-GridSelect {
 	param (
-		[parameter(Mandatory=$True)] $DataSet,
-		[parameter(Mandatory=$True)][string] $Title,
-		[parameter()][string][ValidateSet('Single','Multiple')]$OutputMode = 'Single'
+		[parameter(ValueFromPipeline=$True, Mandatory=$True)] $InputObject,
+		[parameter(Mandatory=$False)][string] $Title,
+		[parameter(Mandatory=$False)][string][ValidateSet('Single','Multiple')] $OutputMode = 'Single',
+		[parameter(Mandatory=$False)][switch] $ForceConsole
 	)
-	if ($IsLinux) {
-		if (Get-Module microsoft.powershell.consoleguitools -listavailable) {
-			@($DataSet | Out-ConsoleGridView -Title $Title -OutputMode $OutputMode)
+	if ([string]::IsNullOrWhiteSpace($Title)) {
+		if ($OutputMode -eq 'Single') {
+			$Title = "Select Item"
+		} else {
+			$Title = "Select Items"
+		}
+	}
+	if ($IsLinux -eq $True) {
+		if (Get-Module microsoft.powershell.consoleguitools -ListAvailable) {
+			$InputObject | Out-ConsoleGridView -Title $Title -OutputMode $OutputMode
 		} else {
 			Write-Warning "Linux platforms require module: microsoft.powershell.consoleguitools"
 		}
 	} else {
-		if (Get-Module microsoft.powershell.consoleguitools -listavailable) {
-			@($DataSet | Out-ConsoleGridView -Title $Title -OutputMode $OutputMode)
+		if ($ForceConsole.IsPresent -and (Get-Module Microsoft.PowerShell.ConsoleGuiTools -ListAvailable)) {
+			$InputObject | Out-ConsoleGridView -Title $Title -OutputMode $OutputMode
 		} else {
-			@($DataSet | Out-GridView -Title $Title -OutputMode $OutputMode)
+			$InputObject | Out-GridView -Title $Title -OutputMode $OutputMode
 		}
 	}
 }
