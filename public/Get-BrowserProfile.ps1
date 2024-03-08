94e12cd856d1d343d9d7c1ef45f78e8deff48403
Skatterbrainz <Skatterbrainz@users.noreply.github.com>
2024-03-08 00:52:13 -0500

    Fixed linux refs for Chrome and Edge. Broke Windows

--- public/Get-BrowserProfile.ps1
+++ public/Get-BrowserProfile.ps1
@@ -6,155 +6,76 @@ function Get-BrowserProfile {
 		Query Browser profiles, returning ID, Name and UserName. For current user or all users
 	.PARAMETER Browser
 		Optional. Browser app to target for query (if installed)
-		* Default = Get default browser from registry query
 		* Chrome
 		* Edge
-		* Brave
-		* Firefox
 	.PARAMETER ProfileName
 		Return information on one specific profile only
-	.PARAMETER PreferenceDetails
-		If ProfileName is specified, will return the Preferences object
-	.PARAMETER AllUsers
-		Query all users on the computer
-	.EXAMPLE
-		Get-BrowserProfile
 	.EXAMPLE
 		Get-BrowserProfile -Browser Edge
-	.EXAMPLE
-		Get-BrowserProfile -Browser Chrome -AllUsers
-	.EXAMPLE
-		Get-BrowserProfile -ProfileName "Azure" -PreferenceDetails
 	.LINK
 		https://github.com/Skatterbrainz/helium/blob/master/docs/Get-BrowserProfile.md
 	#>
 	[CmdletBinding()]
 	param (
-		[parameter(Mandatory=$False)][string]$ProfileName = "",
-		[parameter(Mandatory=$False)][switch]$PreferenceDetails,
-		[parameter(Mandatory=$False)][string][ValidateSet('Chrome','Edge','Brave','Firefox','Default')]$Browser = 'Default',
-		[parameter(Mandatory=$False)][switch]$AllUsers
+		[parameter(Mandatory=$True)][string][ValidateSet('Chrome','Edge')]$Browser,
+		[parameter(Mandatory=$False)][string]$ProfileName,
+		[parameter(Mandatory=$False)][switch]$PreferenceDetails
 	)
-	$results = @()
-	if (!$AllUsers) {
-		if ($Browser -eq 'Default') {
-			$app = Get-DefaultBrowser
-		} else {
-			$app = $Browser
-		}
-		switch ($app) {
-			'Chrome' {
-				$profilePath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
-			}
-			'Brave' {
-				$profilePath = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data"
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
-			}
-			'Edge' {
-				$profilePath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data"
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
-			}
-			'Firefox' {
-				$profilePath = "$env:APPDATA\Mozilla\Firefox\Profiles"
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory | select-object Name,FullName
-			}
-		}
-		if ($app -ne 'Firefox') {
-			foreach ($folder in $profileFolders) {
-				[string]$pref = Join-Path $folder.FullName "Preferences"
-				if (Test-Path $pref) {
-					$prefdata = Get-Content $pref -Raw | ConvertFrom-Json
-					$pname = $prefdata.profile.name
-					$results += [pscustomobject]@{
-						UserName    = $env:USERNAME
-						ProfileID   = $folder.Name
-						ProfileName = $pname
-						Browser     = $app
-						Path        = $folder.FullName
-					}
-				}
-			}
-		} else {
-			foreach ($folder in $profileFolders) {
-				$prefdata = $folder.Name.Split('.')
-				$pname = $prefdata[1]
-				$results += [pscustomobject]@{
-					UserName    = $env:USERNAME
-					ProfileID   = $folder.Name
-					ProfileName = $pname
-					Browser     = $app
-					Path        = $folder.FullName
-				}
-			}
-		}
-	} else {
-		$exclude = @('AppData','Public')
-		if ($Browser -eq 'Default') {
-			$app = Get-DefaultBrowser
-		} else {
-			$app = $Browser
-		}
-		switch ($app) {
-			'Chrome' {
-				$profileSub = "AppData\Google\Chrome\User Data"
-			}
-			'Brave' {
-				$profileSub = "AppData\Local\BraveSoftware\Brave-Browser\User Data"
-			}
-			'Edge' {
-				$profileSub = "AppData\Local\Microsoft\Edge\User Data"
-			}
-			'Firefox' {
-				$profileSub = 'AppData\Local\Mozilla\Firefox\Profiles'
-			}
-		}
-		[array]$userprofiles = Get-ChildItem -Path "c:\users" -Directory | Where-Object {$_.name -notin $exclude} | Select-Object Name,FullName
-		Write-Verbose "$($userprofiles.Count) user profiles found"
-		foreach ($userpath in $userprofiles) {
-			$profilePath = Join-Path $($userpath.FullName) $profileSub
-			Write-Verbose "reading: $profilePath"
-			if ($app -ne 'Firefox') {
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory -Filter "Profile*" | select-object Name,FullName
-				foreach ($folder in $profileFolders) {
-					[string]$pref = Join-Path $folder.FullName "Preferences"
-					if (Test-Path $pref) {
-						$prefdata = Get-Content $pref -Raw | ConvertFrom-Json
-						$pname = $prefdata.profile.name
-						$results += [pscustomobject]@{
-							UserName    = $userpath.Name
-							ProfileID   = $folder.Name
-							ProfileName = $pname
-							Browser     = $app
-							Path        = $folder.FullName
+	switch ($Browser) {
+		'Chrome' {
+			if ($IsLinux) {
+				$rootpath = '~/.var/app/com.google.Chrome/config/google-chrome'
+				$profpath = "$rootpath/Local State"
+				if (!(Test-Path $profpath)) { throw "File not found: $profpath" }
+				$profileData = Get-Content $profpath| ConvertFrom-Json
+				$buildinfo   = Get-Content '~/.var/app/com.google.Chrome/config/google-chrome/Last Version'
+				$profiles    = $profileData.profile.info_cache.psobject.Properties.Name
+				foreach ($profile in $profiles) {
+					$pname = $profileData.profile.info_cache."$profile".name
+					if (([string]::IsNullOrWhiteSpace($ProfileName)) -or ((![string]::IsNullOrWhiteSpace($ProfileName)) -and $ProfileName -eq $pname)) {
+						[pscustomobject]@{
+							ProfileID    = $profile
+							Path         = Join-Path $rootpath $profile
+							Name         = $pname
+							Browser      = $Browser
+							Version      = $buildinfo
+							ComputerName = $(hostname)
+							UserName     = $($env:USER)
 						}
 					}
 				}
 			} else {
-				[array]$profileFolders = Get-ChildItem -Path $profilePath -Directory | select-object Name,FullName
-				foreach ($folder in $profileFolders) {
-					$prefdata = $folder.Name.Split('.')
-					$pname = $prefdata[1]
-					$results += [pscustomobject]@{
-						UserName    = $env:USERNAME
-						ProfileID   = $folder.Name
-						ProfileName = $pname
-						Browser     = $app
-						Path        = $folder.FullName
-					}
-				}
+				$rootpath = "$env:LOCALAPPDATA\Google\Chrome\User Data"
+				# NEED TO REDO THIS SECTION
 			}
 		}
-	}
-	if (![string]::IsNullOrWhiteSpace($ProfileName)) {
-		if ($PreferenceDetails) {
-			$ppath = $results | Where-Object {$_.ProfileName -eq $ProfileName} | Select-Object -ExpandProperty Path
-			$pref = Join-Path $ppath "Preferences"
-			Get-Content -Path $pref | ConvertFrom-Json
-		} else {
-			$results | Where-Object {$_.ProfileName -eq $ProfileName}
+		'Edge' {
+			if ($IsLinux) {
+				$rootpath = '~/.var/app/com.microsoft.Edge/config/microsoft-edge'
+				$profpath = "$rootpath/Local State"
+				if (!(Test-Path $profpath)) { throw "File not found: $profpath" }
+				$profileData = Get-Content $profpath | ConvertFrom-Json
+				$buildinfo   = Get-Content '~/.var/app/com.microsoft.Edge/config/microsoft-edge/Last Version'
+				$profiles    = $profileData.profile.info_cache.psobject.Properties.Name
+				foreach ($profile in $profiles) {
+					$pname = $profileData.profile.info_cache."$profile".name
+					if (([string]::IsNullOrWhiteSpace($ProfileName)) -or ((![string]::IsNullOrWhiteSpace($ProfileName)) -and $ProfileName -eq $pname)) {
+						[pscustomobject]@{
+							ProfileID    = $profile
+							Path         = Join-Path $rootpath $profile
+							Name         = $pname
+							Browser      = $Browser
+							Version      = $buildinfo
+							ComputerName = $(hostname)
+							UserName     = $($env:USER)
+						}
+					}
+				}
+			} else {
+				$rootpath = "$env:LOCALAPPDATA\Microsoft\Edge\User Data"
+				# NEED TO REDO THIS SECTION
+			}
 		}
-	} else {
-		$results
+		# might add Brave and other Chromium-based options later. Firefox dropped profiles so they won't be added
 	}
 }
